65db1be694b3a8a7362534c6bac6912571148b61
stich2005 <orlan2005@gmail.com>
2024-09-24 11:22:39 +1000

    Create maximumWealth.py

---
+++ maximumWealth.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        n = len(accounts)
+        max = 0
+        for i in range(n):
+            sum = 0
+            for j in range(len(accounts[i])):
+                sum += accounts[i][j]
+            if sum > max:
+                max = sum
+        return max
