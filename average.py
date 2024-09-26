0868dacc7a32c4e3f7e8daf22551fb8ba53e8feb
stich2005 <orlan2005@gmail.com>
2024-09-26 16:06:12 +1000

    Update average.py

--- average.py
+++ average.py
@@ -1,8 +1,8 @@
 #1491. Average salary excluding the minimum and maximum salary
 def average(self, salary: List[int]) -> float:
   iMin = min(salary)
-    iMax = max(salary)
-    salary.remove(iMin)
-    salary.remove(iMax)
-    iSum = sum(salary)
-    return iSum/len(salary)
+  iMax = max(salary)
+  salary.remove(iMin)
+  salary.remove(iMax)
+  iSum = sum(salary)
+  return iSum/len(salary)
